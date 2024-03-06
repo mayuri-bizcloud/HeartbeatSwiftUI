76732241a687d17ca46980d5faca87584009cf81
mayuri-bizcloud <mayuri.prasad@bizcloudexperts.com>
2024-03-05 20:53:24 -0600

    Initial Commit

---
+++ HeartbeatSwiftUI Watch App/HeartbeatSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartbeatSwiftUIApp.swift
+//  HeartbeatSwiftUI Watch App
+//
+//  Created by Work on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartbeatSwiftUI_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
